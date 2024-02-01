f93ca1694db44627e9ef8003d11a30844979460d
SpikeHD <spikegdofficial@gmail.com>
2024-02-01 14:14:57 -0800

    feat(all): request failure alert

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -5,6 +5,35 @@ import './style.css'
 import { randomRGBAColors } from '../../util/colors'
 import { DataHome } from './data'
 import PluginsSection from '../../components/Plugins/PluginsSection'
+import { ProcchiAlert } from '../../components/alert/Alert'
+
+let failedRequests = 0
+
+const handleFailure = () => {
+  failedRequests++
+
+  if (failedRequests % 5 === 0) {
+    window.dispatchEvent(ProcchiAlert(`Failed to fetch data from server (${failedRequests} failed requests). Has Procchi or your server crashed?`))
+  }
+
+  throw new Error('Failed to fetch')
+}
+
+const procchiFetch = async (url: string) => {
+  const res = await fetch(url).catch((e) => {
+    console.error(e)
+    handleFailure()
+  })
+
+  if (!res || !res.ok) {
+    handleFailure()
+    return null
+  }
+
+  failedRequests = 0
+
+  return res
+}
 
 export function Home() {
   const [sysinfo, setSysinfo] = useState({} as SystemInfo)
@@ -19,23 +48,23 @@ export function Home() {
 
   useEffect(() => {
     async function grabLatestData(firstTime = false) {
-      const mres = await fetch('/api/memory')
+      const mres = await procchiFetch('/api/memory')
       const mdata = await mres.json()
       setMemoryData(mdata)
 
-      const sres = await fetch('/api/swap')
+      const sres = await procchiFetch('/api/swap')
       const sdata = await sres.json()
       setSwapData(sdata)
 
-      const cres = await fetch('/api/cpu')
+      const cres = await procchiFetch('/api/cpu')
       const cdata = await cres.json()
       setCpuData(cdata)
 
-      const dres = await fetch('/api/disk')
+      const dres = await procchiFetch('/api/disk')
       const ddata = await dres.json()
       setDiskData(ddata)
 
-      const nres = await fetch('/api/network')
+      const nres = await procchiFetch('/api/network')
       const ndata = await nres.json()
       setNetworkData(ndata)
 
